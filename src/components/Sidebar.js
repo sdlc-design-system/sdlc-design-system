56d1cdffd2559a0054600a981ca6a408beca7c28
justJosh1004 <j.joson@live.com>
2019-06-24 11:42:33 -0400

    added border on sidebar nav click. some div cleanup to fix display issues with adding right border

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,61 +2,172 @@ import React from 'react';
 import './componentStyles/Sidebar.scss';
 
 class Sidebar extends React.Component {
-  constructor ( props ) {
-    super( props );
+  constructor(props) {
+    super(props);
     this.state = {
       showForms: false,
       showContainers: false,
       showContent: false,
-    }
+      activeTab: 'colors'
+    };
   }
-  handleContainerClick=()=> {
-    this.setState({showContainers : !(this.state.showContainers) });
-  }
-  handleFormsClick=()=> {
-    this.setState({showForms : !(this.state.showForms) });
-  }  
-  handleContentClick=()=> {
-    this.setState({showContent : !(this.state.showContent) });
-  }
+  handleContainerClick = () => {
+    this.setState({ showContainers: !this.state.showContainers });
+  };
+  handleFormsClick = () => {
+    this.setState({ showForms: !this.state.showForms });
+  };
+  handleContentClick = () => {
+    this.setState({ showContent: !this.state.showContent });
+  };
 
-  render () {
-    return(
-      <div>
-        <div className="sideVerticalLine"></div>
-        <div className="disappearingSideBar">
-          <div><a href="#colors">Colors</a></div>
-          <div><a href="#typography">Typography</a></div>
-          <div><a href="#icons">Icons</a></div>
-          <div><a href="#buttons">Buttons</a></div>
-          <div onClick={this.handleFormsClick}><a href="#forms">Forms</a></div>
-          {this.state.showForms ?
-            <div className="indentSidebar">
-              <div><a href="#standardInput">Standard Text Input</a></div>
-              <div><a href="#dropdownMenu">Dropdown Menu</a></div>
-              <div><a href="#radioButtons">Radio Buttons</a></div>
-              <div><a href="#checkbox">Checkboxes</a></div>
-              <div><a href="#switch">Switch</a></div>
-            </div>
-          :""}
-          <div onClick={this.handleContainerClick}><a href="#containers">Containers</a></div>
-          {this.state.showContainers ?
-            <div className="indentSidebar">
-              <div><a href="#panel">Panel</a></div>
-              <div><a href="#accordion">Accordion</a></div>
-              <div><a href="#modal">Modal</a></div>
-            </div>
-          :""}
-          <div onClick={this.handleContentClick}><a href="#content">Content</a></div>
-          {this.state.showContent ?
-          <div className="indentSidebar">
-            <div><a href="#dividers">Dividers</a></div>
-            <div><a href="#loadingIndicator">Loading Indicator</a></div>
-          </div>
-          :""}
+  handleClick = item => {
+    if (this.state.activeTab !== item)
+      this.setState({ ...this.state, activeTab: item });
+  };
+
+  render() {
+    return (
+      <div className="sideVerticalLine disappearingSideBar">
+        {/* <div className="sideVerticalLine" /> */}
+        {/* <div className="sideVerticalLine disappearingSideBar"> */}
+        <div className={this.state.activeTab === 'colors' ? 'active' : ''}>
+          <a href="#colors" onClick={() => this.handleClick('colors')}>
+            Colors
+          </a>
         </div>
+        <div className={this.state.activeTab === 'typography' ? 'active' : ''}>
+          <a href="#typography" onClick={() => this.handleClick('typography')}>
+            Typography
+          </a>
+        </div>
+        <div className={this.state.activeTab === 'icons' ? 'active' : ''}>
+          <a href="#icons" onClick={() => this.handleClick('icons')}>
+            Icons
+          </a>
+        </div>
+        <div className={this.state.activeTab === 'buttons' ? 'active' : ''}>
+          <a href="#buttons" onClick={() => this.handleClick('buttons')}>
+            Buttons
+          </a>
+        </div>
+        <div
+          onClick={this.handleFormsClick}
+          className={this.state.activeTab === 'forms' ? 'active' : ''}>
+          <a href="#forms" onClick={() => this.handleClick('forms')}>
+            Forms
+          </a>
+        </div>
+        {this.state.showForms ? (
+          <div className="indentSidebar">
+            <div
+              className={
+                this.state.activeTab === 'standardInput' ? 'active' : ''
+              }>
+              <a
+                href="#standardInput"
+                onClick={() => this.handleClick('standardInput')}>
+                Standard Text Input
+              </a>
+            </div>
+            <div
+              className={
+                this.state.activeTab === 'dropdownMenu' ? 'active' : ''
+              }>
+              <a
+                href="#dropdownMenu"
+                onClick={() => this.handleClick('dropdownMenu')}>
+                Dropdown Menu
+              </a>
+            </div>
+            <div
+              className={
+                this.state.activeTab === 'radiButtons' ? 'active' : ''
+              }>
+              <a
+                href="#radioButtons"
+                onClick={() => this.handleClick('radiButtons')}>
+                Radio Buttons
+              </a>
+            </div>
+            <div
+              className={this.state.activeTab === 'checkBox' ? 'active' : ''}>
+              <a href="#checkbox" onClick={() => this.handleClick('checkBox')}>
+                Checkboxes
+              </a>
+            </div>
+            <div className={this.state.activeTab === 'switch' ? 'active' : ''}>
+              <a href="#switch" onClick={() => this.handleClick('switch')}>
+                Switch
+              </a>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+        <div
+          onClick={this.handleContainerClick}
+          className={this.state.activeTab === 'containers' ? 'active' : ''}>
+          <a href="#containers" onClick={() => this.handleClick('containers')}>
+            Containers
+          </a>
+        </div>
+        {this.state.showContainers ? (
+          <div className="indentSidebar">
+            <div className={this.state.activeTab === 'panel' ? 'active' : ''}>
+              <a href="#panel" onClick={() => this.handleClick('panel')}>
+                Panel
+              </a>
+            </div>
+            <div
+              className={this.state.activeTab === 'accordion' ? 'active' : ''}>
+              <a
+                href="#accordion"
+                onClick={() => this.handleClick('accordion')}>
+                Accordion
+              </a>
+            </div>
+            <div className={this.state.activeTab === 'modal' ? 'active' : ''}>
+              <a href="#modal" onClick={() => this.handleClick('modal')}>
+                Modal
+              </a>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+        <div
+          onClick={this.handleContentClick}
+          className={this.state.activeTab === 'content' ? 'active' : ''}>
+          <a href="#content" onClick={() => this.handleClick('content')}>
+            Content
+          </a>
+        </div>
+        {this.state.showContent ? (
+          <div className="indentSidebar">
+            <div
+              className={this.state.activeTab === 'dividers' ? 'active' : ''}>
+              <a href="#dividers" onClick={() => this.handleClick('dividers')}>
+                Dividers
+              </a>
+            </div>
+            <div
+              className={
+                this.state.activeTab === 'loadingIndicator' ? 'active' : ''
+              }>
+              <a
+                href="#loadingIndicator"
+                onClick={() => this.handleClick('loadingIndicator')}>
+                Loading Indicator
+              </a>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+        {/* </div> */}
       </div>
     );
   }
 }
-export default Sidebar
+export default Sidebar;
