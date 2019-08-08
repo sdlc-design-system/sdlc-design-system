6567440b7f2c65fa310d4502a0f13827a961810b
justJosh1004 <j.joson@live.com>
2019-08-08 14:44:27 -0400

    panel label added labels16 in classname. heavyDivider changed to 1px

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../App.scss'
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import '../App.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './componentStyles/Panel.scss';
 
 // see https://react-bootstrap.github.io/utilities/transitions/#collapse-props
@@ -9,48 +9,51 @@ class Panel extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      openTab: [true,...Array(this.props.content.length-1).fill(false)],
+      openTab: [true, ...Array(this.props.content.length - 1).fill(false)],
       clearChoices: Array(this.props.content.length).fill(false)
-    }
-    this.props.content.forEach(()=>{
-      this.setState({openTab: [...this.state.openTab,false]})
-
-    })
+    };
+    this.props.content.forEach(() => {
+      this.setState({ openTab: [...this.state.openTab, false] });
+    });
   }
-  handleTabChange =(index)=>{
-    const openTab = [...this.state.clearChoices]
-    openTab[index] = !openTab[index]
+  handleTabChange = index => {
+    const openTab = [...this.state.clearChoices];
+    openTab[index] = !openTab[index];
     this.setState({
       openTab
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div>
         <Row>
-          {this.props.content.map((item,index)=>
-            <Col key={index} className="tabs" onClick={()=>this.handleTabChange(index)}>
-              <span style={{cursor:"pointer"}}>
-                {item.title}
-              </span>
-              <hr style={{
-                backgroundColor: "#44687D",  
-                height: "3px",
-                opacity: this.state.openTab[index] ? 1:0,
-                transition: ".3s ease-in-out",
-
-                }}>
-              </hr>
+          {this.props.content.map((item, index) => (
+            <Col
+              key={index}
+              className="tabs labels16"
+              onClick={() => this.handleTabChange(index)}>
+              <span style={{ cursor: 'pointer' }}>{item.title}</span>
+              <hr
+                className="heavyDivider"
+                style={{
+                  // backgroundColor: '#44687D',
+                  // height: '3px',
+                  opacity: this.state.openTab[index] ? 1 : 0
+                  //   transition: '.3s ease-in-out'
+                }}
+              />
             </Col>
-          )}
+          ))}
         </Row>
-        <hr className="lightDivider lineUnderLabels"></hr>
-        {this.props.content.map((item,index)=>
-          <div key={index}>{this.state.openTab[index] ? item.content+' ' : ''}</div>
-        )}        
+        <hr className="lightDivider lineUnderLabels" />
+        {this.props.content.map((item, index) => (
+          <div key={index}>
+            {this.state.openTab[index] ? item.content + ' ' : ''}
+          </div>
+        ))}
       </div>
     );
   }
 }
-export default Panel
+export default Panel;
